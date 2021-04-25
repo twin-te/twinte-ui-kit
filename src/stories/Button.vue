eab2e5c6b98257f2b38568ab284082ff2f83edaa
HikaruEgashira <shighten@gmail.com>
2021-04-25 23:06:23 +0900

    init vue3

---
+++ src/stories/Button.vue
@@ -0,0 +1,51 @@
+<template>
+  <button type="button" :class="classes" @click="onClick" :style="style">
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import "./button.css";
+import { reactive, computed, PropType } from "vue";
+
+export default {
+  name: "my-button",
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String as PropType<"small" | "medium" | "large">,
+    },
+    backgroundColor: {
+      type: String,
+    },
+  },
+
+  emits: ["click"],
+
+  setup(props, { emit }) {
+    props = reactive(props);
+    return {
+      classes: computed(() => ({
+        "storybook-button": true,
+        "storybook-button--primary": props.primary,
+        "storybook-button--secondary": !props.primary,
+        [`storybook-button--${props.size || "medium"}`]: true,
+      })),
+      style: computed(() => ({
+        backgroundColor: props.backgroundColor,
+      })),
+      onClick() {
+        emit("click");
+      },
+    };
+  },
+};
+</script>
